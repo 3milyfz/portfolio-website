9416dac8942ad28e3ea63f2820b92ccb90804022
3m1ly03 <emilyfaithzhou@icloud.com>
2023-12-19 17:16:30 -0500

    Made more opaque on mobile

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -28,8 +28,8 @@ const Card = styled.div`
 
   @media only screen and (max-width: 768px) {
     width: 80%;
-    background-color: rgb(255 255 255 / 5%);
-    box-shadow: 0 0 5px rgb(0 0 0 / 15%);
+    background-color: rgb(255 255 255 / 15%);
+    box-shadow: 0 0 5px rgb(0 0 0 / 30%);
     backdrop-filter: blur(30px);
   }
 `;
