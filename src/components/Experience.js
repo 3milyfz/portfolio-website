4943a7800e20b2a2992bf65bd133256b86a3af88
3m1ly03 <emilyfaithzhou@icloud.com>
2023-12-25 14:27:09 -0500

    Fixed missing dependency

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -95,7 +95,7 @@ const Experience = () => {
         };
       }
     });
-  }, []);
+  }, [cardRefs]);
 
   return (
     <Section>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -84,7 +84,7 @@ const Projects = () => {
         };
       }
     });
-  }, []);
+  }, [cardRefs]);
 
   return (
     <Section>
