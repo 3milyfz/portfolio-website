7b54f770c2ddedfba201c14f93cfec50390a5d7b
3m1ly03 <emilyfaithzhou@icloud.com>
2024-01-05 23:13:03 -0500

    Modified max bubbles on smaller screens

--- src/App.js
+++ src/App.js
@@ -7,38 +7,41 @@ import Contact from './components/Contact';
 
 function App() {
   const [bubbles, setBubbles] = useState([]);
+  const maxBubbles = window.innerWidth < 768 ? 10 : 20; // Adjust the number of bubbles for smaller screens
 
   useEffect(() => {
     const createBubble = () => {
       const minSize = 20;
-      const maxSize = 180; // Adjust the maximum size as needed
+      const maxSize = 150;
       const size = minSize + Math.random() * (maxSize - minSize);
       const left = Math.random() * window.innerWidth + 'px';
 
-      const newBubble = {
-        size: `${size}px`,
-        left: left,
-        key: Date.now(),
-      };
+      if (bubbles.length < maxBubbles) {
+        const newBubble = {
+          size: `${size}px`,
+          left: left,
+          key: Date.now(),
+        };
 
-      setBubbles((prevBubbles) => [...prevBubbles, newBubble]);
+        setBubbles((prevBubbles) => [...prevBubbles, newBubble]);
 
-      setTimeout(() => {
-        setBubbles((prevBubbles) =>
-          prevBubbles.map((bubble) =>
-            bubble.key === newBubble.key ? { ...bubble, removing: true } : bubble
-          )
-        );
         setTimeout(() => {
-          setBubbles((prevBubbles) => prevBubbles.filter((bubble) => bubble.key !== newBubble.key));
-        }, 500); // Adjust the time to match the "bubblePop" animation duration
-      }, 4000);
+          setBubbles((prevBubbles) =>
+            prevBubbles.map((bubble) =>
+              bubble.key === newBubble.key ? { ...bubble, removing: true } : bubble
+            )
+          );
+          setTimeout(() => {
+            setBubbles((prevBubbles) => prevBubbles.filter((bubble) => bubble.key !== newBubble.key));
+          }, 500); // Adjust the time to match the "bubblePop" animation duration
+        }, 4000);
+      }
     };
 
-    const bubbleInterval = setInterval(createBubble, 180);
+    const bubbleInterval = setInterval(createBubble, 150);
 
     return () => clearInterval(bubbleInterval);
-  }, []);
+  }, [bubbles, maxBubbles]);
 
   return (
     <div className="App">
