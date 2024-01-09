b7712afffc997b541dd9bfabc63907c35684563f
3m1ly03 <emilyfaithzhou@icloud.com>
2024-01-08 19:46:35 -0500

    Switched copyright side

--- src/components/Copyright.js
+++ src/components/Copyright.js
@@ -2,19 +2,20 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Container = styled.div`
+    position: fixed; // Make it fixed position
+    bottom: 0;
     height: fit-content;
     padding: 10px;
     background-color: black;
-    text-align: left;
+    text-align: right;
     width: 100%;
     z-index: 1000; // High value to ensure it's on top
-    position: fixed; // Make it fixed position
-    bottom: 0;
 `;
 
 const Text = styled.p`
     color: rgb(210, 212, 199);
     font-size: 18px;
+    padding-right: 25px;
 `;
 
 const Copyright = () => {
