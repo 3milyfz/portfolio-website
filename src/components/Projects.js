b417c27e09c8716d13269961bf156fe073482585
Emily Zhou <73791848+3milyfz@users.noreply.github.com>
2024-06-24 10:07:14 -0400

    Updated padding for projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -9,8 +9,8 @@ const Section = styled.div`
   height: fit-content;
   min-height: 100vh;
   gap: 20px;
-  padding-left: 40px;
-  padding-right: 40px;
+  padding-left: 55px;
+  padding-right: 55px;
 
   @media only screen and (max-width: 768px) {
     height: fit-content;
